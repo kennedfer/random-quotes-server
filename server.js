44a87353c2c4e8e899ccbb581723fb7ca1eb5851
kennedfer <kenned14santos@gmail.com>
2023-01-21 14:07:00 -0300

    req

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 5000; 
 
-app.use((res, next) => {
+app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*']);
   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.append('Access-Control-Allow-Headers', 'Content-Type');
