5783653e245cbdfc8777bcf0b00208f6b68f63a1
kennedfer <kenned14santos@gmail.com>
2023-01-21 13:55:34 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express'; 
+
+const app = express();
+const port = process.env.PORT || 5000; 
+
+app.use((res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
+app.get('/get-random-quote', async (req, res) => {
+  let data = await fetch('https://zenquotes.io/api/random');
+  let resp = await data.json();
+  res.json(resp);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
