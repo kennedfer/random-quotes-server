b09bc44082aca5e28ed5a725662a678ae2e03aab
kennedfer <kenned14santos@gmail.com>
2023-01-21 14:28:50 -0300

    log

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use((req, res, next) => {
 
 
 app.get('/get-random-quote', async (req, res) => {
+  console.log('api fething');
   let data = await fetch('https://zenquotes.io/api/random');
   let resp = await data.json();
   res.json(resp);
